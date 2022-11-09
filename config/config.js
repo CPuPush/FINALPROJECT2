67b1ed148a84765baaa435d1e32dcf88df505205
CPuPush <fpakpahan44@gmail.com>
2022-11-09 16:18:49 +0700

    PRODUCTION

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "poisedon4",
+    "database": "final_project_2",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.PGUSER,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "port": process.env.PGPORT,
+    "host": process.env.PGHOST,
+    "dialect": "postgres"
+  }
+}
